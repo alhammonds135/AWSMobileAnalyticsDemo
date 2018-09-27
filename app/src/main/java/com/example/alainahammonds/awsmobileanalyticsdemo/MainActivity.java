a7dacdacd8050e7b9fd351a7bfa12bdff969cc55
Alaina Hammonds <ahammonds@onetrust.com>
2018-09-27 16:21:19 -0400

    Revert File to Basic

--- app/src/main/java/com/example/alainahammonds/awsmobileanalyticsdemo/MainActivity.java
+++ app/src/main/java/com/example/alainahammonds/awsmobileanalyticsdemo/MainActivity.java
@@ -6,9 +6,6 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import com.amazonaws.mobile.client.AWSMobileClient;
-import com.amazonaws.mobile.client.AWSStartupHandler;
-import com.amazonaws.mobile.client.AWSStartupResult;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,13 +16,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        AWSMobileClient.getInstance().initialize(this, new AWSStartupHandler() {
-            @Override
-            public void onComplete(AWSStartupResult awsStartupResult) {
-                Log.d("YourMainActivity", "AWSMobileClient is instantiated and you are connected to AWS!");
-            }
-        }).execute();
 
         setContentView(R.layout.activity_main);
         button1 = findViewById(R.id.button);
