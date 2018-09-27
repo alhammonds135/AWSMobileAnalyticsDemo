2734795547c224c9946cdc3e040335aae93abd99
Alaina Hammonds <ahammonds@onetrust.com>
2018-09-27 00:18:12 -0400

    Add AWS YEET

--- app/src/main/java/com/example/alainahammonds/awsmobileanalyticsdemo/MainActivity.java
+++ app/src/main/java/com/example/alainahammonds/awsmobileanalyticsdemo/MainActivity.java
@@ -2,12 +2,38 @@ package com.example.alainahammonds.awsmobileanalyticsdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import com.amazonaws.mobile.client.AWSMobileClient;
+import com.amazonaws.mobile.client.AWSStartupHandler;
+import com.amazonaws.mobile.client.AWSStartupResult;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button1;
+    TextView textView;
+    int count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        AWSMobileClient.getInstance().initialize(this, new AWSStartupHandler() {
+            @Override
+            public void onComplete(AWSStartupResult awsStartupResult) {
+                Log.d("YourMainActivity", "AWSMobileClient is instantiated and you are connected to AWS!");
+            }
+        }).execute();
+
         setContentView(R.layout.activity_main);
+        button1 = findViewById(R.id.button);
+        textView = findViewById(R.id.textView);
+    }
+
+    public void onButtonClick(View view) {
+        count++;
+        textView.setText("Clicked " + count + " times");
     }
 }
